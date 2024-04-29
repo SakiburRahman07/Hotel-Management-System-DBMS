1551e335e8efd59667757dbeac56b07a4e3af17a
sakib <sakiburrahman11.msr@gmail.com>
2024-04-29 23:50:53 +0600

    Trigger

--- Trigger.sql
+++ Trigger.sql
@@ -1,3 +1,33 @@
+--trigger for deletion
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER delete_guest
+BEFORE delete ON guest 
+REFERENCING OLD AS o NEW AS n
+FOR EACH ROW
+BEGIN
+delete from event_in_hotel where guest_id=:o.guest_id;
+END;
+/
+
+delete from guest where guest_id=3;
+select * from guest;
+--trigger for insertion
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER updateevent
+AFTER UPDATE ON event
+REFERENCING OLD AS o NEW AS n
+FOR EACH ROW
+BEGIN
+    UPDATE event_in_hotel
+    SET event_id = :n.event_id
+    WHERE event_id = :o.event_id;
+    
+END;
+/
+
+update event set event_id=1 where event_id=9;
+select * from event_in_hotel;
+
 -- Creating a trigger to track room's availability in case a room was reserved
 CREATE OR REPLACE TRIGGER room_registry_trg
 AFTER INSERT ON room_reservation
