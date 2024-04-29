16fd17a876b9f120a16eb695608650139d0beed9
sakib <sakiburrahman11.msr@gmail.com>
2024-04-29 23:26:33 +0600

    Function and call it

--- PL-SQL.sql
+++ PL-SQL.sql
@@ -70,3 +70,22 @@ begin
   END LOOP;
 end;
 /
+
+--function
+set serveroutput on
+create or replace function eventshow(var1 in varchar) return varchar AS
+value event.event_name%type;
+begin
+  select event_name into value from event where event_id=var1; 
+  DBMS_OUTPUT.PUT_LINE(value);
+   return value;
+end;
+/
+--call the function
+set serveroutput on
+declare 
+value varchar(100);
+begin
+value:=eventshow(3);
+end;
+/
